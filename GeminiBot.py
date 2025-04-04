f0b6364a04f98fb3c943dffcfa3a79e1cb2733ac
Alexjspark0 <alexjspark0@gmail.com>
2025-04-03 22:52:46 -0400

    w

--- GeminiBot.py
+++ GeminiBot.py
@@ -1,58 +1,31 @@
-import base64
 import os
-from google import genai
-from google.genai import types
+import google.generativeai as genai
+from dotenv import load_dotenv
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
+# Create the model
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
 
-def generate():
-    client = genai.Client(
-        api_key=os.environ.get(AIzaSyC7SUXFzcyazJeqwiMueNfOFREV7R1pcF8),
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-pro",
+  generation_config=generation_config,
+)
+
+history = []
+while True:
+    message = input()
+    chat_session = model.start_chat(
+        history=history
     )
-
-    model = "gemini-2.0-flash"
-    contents = [
-        types.Content(
-            role="user",
-            parts=[
-                types.Part.from_text(text="""INSERT_INPUT_HERE"""),
-            ],
-        ),
-    ]
-    generate_content_config = types.GenerateContentConfig(
-        safety_settings=[
-            types.SafetySetting(
-                category="HARM_CATEGORY_HARASSMENT",
-                threshold="BLOCK_MEDIUM_AND_ABOVE",  # Block some
-            ),
-            types.SafetySetting(
-                category="HARM_CATEGORY_HATE_SPEECH",
-                threshold="BLOCK_MEDIUM_AND_ABOVE",  # Block some
-            ),
-            types.SafetySetting(
-                category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
-                threshold="BLOCK_MEDIUM_AND_ABOVE",  # Block some
-            ),
-            types.SafetySetting(
-                category="HARM_CATEGORY_DANGEROUS_CONTENT",
-                threshold="BLOCK_MEDIUM_AND_ABOVE",  # Block some
-            ),
-            types.SafetySetting(
-                category="HARM_CATEGORY_CIVIC_INTEGRITY",
-                threshold="BLOCK_MEDIUM_AND_ABOVE",  # Block some
-            ),
-        ],
-        response_mime_type="text/plain",
-        system_instruction=[
-            types.Part.from_text(text="""Nerdy, Robotic"""),
-        ],
-    )
-
-    for chunk in client.models.generate_content_stream(
-        model=model,
-        contents=contents,
-        config=generate_content_config,
-    ):
-        print(chunk.text, end="")
-
-if __name__ == "__main__":
-    generate()
+    response = chat_session.send_message(message)
+    respond = response.text
+    print(respond)
+    print()
+    history.append({"role":"user","parts":[message]})
+    history.append({"role":"model","parts":[respond]})
